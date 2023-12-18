c2791959d809f24b4db7c5c9435bd7f42c71bc26
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-18 10:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	  let count =0
+  library.forEach((element)=>{
+    element.readingStatus ? count++ : count
+  })
+  return count
+	
 };
 
 // Do not change the code below
